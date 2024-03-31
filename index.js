bd3f4b016461e360c052e683f093fa3a3a53253a
Stephen Gunawan Susilo <gunawanstephen@yahoo.com>
2024-04-01 04:24:51 +0700

    fix server problem d:01042024 s:desktop

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const express = require('express');
 const app = express();
 
 app.use(express.static("public")); // serve files from the public directory
-app.get("/", (req, res) => {
-    const folderPath = "./photos"
+
+// app.get("/", (req,res) => {
+//     res.sendFile("index.html")
+// })
+
+app.get("/data", (req, res) => {
+    const folderPath = "./public/photos"
     const itemNames = fs.readdirSync(folderPath)
     console.log(itemNames)
-    res.json(itemNames) 
+    res.json(itemNames)
 })
+
+
 
 app.listen(3000, () => console.log("server 3000 hahaha"))
 // export module itemNames
--- public/main.js
+++ public/main.js
@@ -1,9 +1,16 @@
-fetch("/").then(response => response.json())
+fetch("/data").then(response => response.json())
 .then(itemNames => {
     itemNames.forEach(itemName => {
         const img = document.createElement("img")
         img.src = `photos/${itemName}`;
         const photoArea = document.getElementsByClassName("photo-area")[0]
         photoArea.appendChild(img)
+        console.log("hmmm")
     });
+}).catch((error) => {
+    console.log("Error:", error)
 });
+
+
+
+// notes: perbaiki layout (css kacau balau)
